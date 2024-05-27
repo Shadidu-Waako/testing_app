60fd8315402904444bbdaae4704d299956ac012b
Waako Shadidu <shadiduwaako@gmail.com>
2024-05-27 12:05:24 +0300

    added scrolling test

--- test/home_test.dart
+++ test/home_test.dart
@@ -25,4 +25,27 @@ void main() {
       expect(find.text('Item 0'), findsNothing);
     });
   });
+
+  group('Home Page Widget Tests', () {
+
+    // BEGINNING OF NEW CONTENT
+    testWidgets('Testing if ListView shows up', (tester) async {  
+      await tester.pumpWidget(createHomeScreen());
+      expect(find.byType(ListView), findsOneWidget);
+    });                                                
+    // END OF NEW CONTENT
+
+      testWidgets('Testing Scrolling', (tester) async {
+        await tester.pumpWidget(createHomeScreen());
+        expect(find.text('Item 0'), findsOneWidget);
+        await tester.fling(
+          find.byType(ListView),
+          const Offset(0, -200),
+          3000,
+        );
+        await tester.pumpAndSettle();
+        expect(find.text('Item 0'), findsNothing);
+      });
+  });
 }
+
